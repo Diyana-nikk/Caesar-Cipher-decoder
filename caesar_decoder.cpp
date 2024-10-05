61542d6616da88a76b145f5a01d7ed62a28eb092
Diyana-nikk <xdiyana.nx@gmail.com>
2024-10-05 10:59:35 +0100

    more progress

--- caesar_decoder.cpp
+++ caesar_decoder.cpp
@@ -6,11 +6,15 @@ using namespace std;
 
 int main()
 {
-char x; 
+string x; 
 cout << "Type your message here: "; // Type a message here and press enter
 cin >> x; // Get user input from the keyboard
   
 const int length = x.length(); 
 char* char_array = new char[length + 1]; 
+
+for (int i = 0; i < x.length(); i++) { 
+        char_array[i] = x[i]; 
+    }
 
 }
