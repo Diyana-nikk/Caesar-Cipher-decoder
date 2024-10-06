129f6ea63a9cf1385a72bc17206a8b78ff9fb837
Diyana-nikk <xdiyana.nx@gmail.com>
2024-10-06 12:27:03 +0100

    finished program and compiled it

--- caesar_decoder.cpp
+++ caesar_decoder.cpp
@@ -1,20 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
 string x; 
-cout << "Type your message here: "; // Type a message here and press enter
-cin >> x; // Get user input from the keyboard
-  
-const int length = x.length(); 
-char* char_array = new char[length + 1]; 
+cout << "Type your message here: \n"; // Type a message here and press enter
+char message[250];
+char output[250];
+cin.getline(message,250); // Get user input from the keyboard
 
-for (int i = 0; i < x.length(); i++) { 
-        char_array[i] = x[i]; 
+int shift;
+
+cout << "Enter the shift value: \n";
+cin>> shift;
+
+const int length = strlen(message); 
+
+char item;
+for (int i = 0; i < length+1; i++) { 
+      item = message[i]; // get ith character of the message
+
+    // if lower case
+    if (item >='a' && item <= 'z'){
+        item= item + shift;
+        if(item>'z'){
+            item=item-'z'+'a'-1; // loop over
+        }
+        output[i]=item;
     }
 
+    //if lowercase:
+    else if (item >='A' && item <='Z'){
+        item = item + shift;
+         if(item>'Z'){
+            item=item-'Z'+'A'-1; // loop over
+        }
+        output[i]=item;
+    }
+    else{
+        output[i]=item;
+    }
+    }
+ cout << "Encrypted message: " << output;
 }
