7ea14b6c655f0eae52b6f7eb76a9fc993b6e0c37
Diyana-nikk <xdiyana.nx@gmail.com>
2024-10-20 17:45:15 +0100

    fixed character and integer overflow

--- caesar_encoder.cpp
+++ caesar_encoder.cpp
@@ -1,74 +1,72 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include <cstring>
+#include <string>  // Use string for dynamic input size
 #include <limits>
 
 using namespace std;
 
-int main()
-{
-char choice;
-bool running = true;
-while (running ){
-    // declare variables:
-    char message[250];
-    char output[250];
+int main() {
+    char choice;
+    bool running = true;
 
-    int shift;
+    while (running) {
+        // Declare variables
+        string message;
+        string output;
+        int shift;
 
-    std::cout << "Type your message here: \n" << endl; // Type a message here and press enter
+        // Get user input for the message
+        cout << "Type your message here: \n" << endl;
+        getline(cin, message); 
 
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Ignore remaining characters in the input buffer
-    cin.getline(message,250); // Get user input from the keyboard
+        // Try getting the shift value
+        cout << "Enter a positive integer shift value: \n" << endl;
+        cin >> shift;
 
-    //try getting the shift value
-    std::cout << "Enter a positive integer shift value: \n" << endl;
-    cin>> shift;
+        // While the input is invalid, make the user re-enter the shift value
+        while (cin.fail() || shift < 0) {
+            cin.clear();  // Clear the error flag on `cin`
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Ignore remaining characters in the input buffer
+            cout << "Nice try, please enter a valid positive integer: \n" << endl;
+            cin >> shift;
+        }
 
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Clear newline after the shift input
 
-    // While the input is invalid, make the user re-enter the shift value
-    while (cin.fail() || shift < 0) {
-        cin.clear();  // Clear the error flag on `cin`
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');  // Ignore remaining characters in the input buffer
-        std::cout << "Nice try, please enter a positive integer: \n" << endl;
-        cin>> shift;
+        // Reduce the shift value modulo 26 to ensure it wraps around the alphabet
+        shift = shift % 26;
+
+        // Encrypt the message
+        for (char item : message) {
+            // If lowercase
+            if (item >= 'a' && item <= 'z') {
+                item = ((item - 'a' + shift) % 26) + 'a';  // Wrap-around for lowercase ASCII characters
+                output += item;
+            }
+            // If uppercase
+            else if (item >= 'A' && item <= 'Z') {
+                item = ((item - 'A' + shift) % 26) + 'A';  // Wrap-around for uppercase
+                output += item;
+            }
+            else {
+                output += item;  // Non-alphabetic characters remain unchanged
+            }
+        }
+
+        cout << "Encrypted message: " << output << endl;
+
+        // Ask the user if they want to run again
+        cout << "Do you want to encode another message? (y/n): " << endl;
+        cin >> choice;
+
+        if (choice != 'y') {
+            running = false;
+        }
+
+        // Clear input buffer for next iteration
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
 
-    const int length = strlen(message); 
+    cout << "See you later!" << endl;
 
-    char item;
-    for (int i = 0; i < length+1; i++) { 
-        item = message[i]; // get ith character of the message
-
-        // if lower case
-    if (item >= 'a' && item <= 'z'){
-                item = ((item - 'a' + shift) % 26) + 'a'; //Wrap-around for lowercase ASKII characters
-                output[i] = item;
-            }
-
-        // if upper case
-            else if (item >= 'A' && item <= 'Z'){
-                item = ((item - 'A' + shift) % 26) + 'A'; //Wrap-around for uppercase
-                output[i]= item;
-            }
-        else{
-            output[i]=item;
-        }
-        }
-    std::cout << "Encrypted message: " << output << endl;
-
-    // Ask the user if they want to run again
-    std::cout << "Do you want to encode another message? (y/n): " << endl;
-    cin >> choice;
-    if (choice != 'y'){
-        running = false;
-    }
-    
-}
-
-    std::cout << "See you later!" << endl;
-
-   return 0;
-
+    return 0;
 }
